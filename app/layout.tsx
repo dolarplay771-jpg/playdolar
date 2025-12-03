23d61f88c82264f3d3b78da847a6699542072993
dolarplay771-jpg <dolarplay771@gmail.com>
2025-12-02 22:06:39 -0300

    121313

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,15 +60,15 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{
             __html: `
               !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1069709421942060');
-              fbq('track', 'PageView');
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1347805550414745');
+fbq('track', 'PageView');
             `,
           }}
         />
@@ -77,7 +77,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1069709421942060&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1347805550414745&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
