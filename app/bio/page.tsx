ba8292cbdcc31f570e64def6febe62106dbbfa2b
dolarplay771-jpg <dolarplay771@gmail.com>
2025-12-08 15:06:24 -0300

    fdvdfvdf

--- app/bio/page.tsx
+++ app/bio/page.tsx
@@ -15,3 +15,4 @@ export default function BioPage() {
   return <Bio />
 }
 
+
--- app/layout.tsx
+++ app/layout.tsx
@@ -59,7 +59,7 @@ export default function RootLayout({
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              !function(f,b,e,v,n,t,s)
+             function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
 if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
